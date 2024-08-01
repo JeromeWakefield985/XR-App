d48ee5fc9b290353ad76149d2f94b2b7e9bd8da2
JeromeWakefield985 <Jeromewakefield985@gmail.com>
2024-07-31 21:09:40 -0400

    wine bottle
    Wine Bottle particle effect

---
+++ XR-App/Assets/myscripts/WinePour.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class WinePour : MonoBehaviour
+{
+
+    public GameObject wineStream;
+    public Transform wineBottle;
+    public Transform bottleTop;
+
+    private bool isPouring = false;
+    private bool pourCheck;
+
+    public float topBottle;
+    public float midBottle;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        topBottle = bottleTop.position.y;
+        midBottle = wineBottle.position.y;
+        pourCheck = midBottle > topBottle;
+
+        if (isPouring != pourCheck)
+        {
+           isPouring = pourCheck;
+           wineStream.SetActive(isPouring);
+        }
+       
+
+
+    }
+}
